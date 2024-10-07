09e7c67b759b3cc52b0b9cc710c14f75f7597211
JhonH-DJ <jhonhardy2003@gmail.com>
2024-10-06 21:32:12 -0500

    Mi comit de trabajo

--- edicion.php
+++ edicion.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enlinea | Diseño</title>
-    <!-- css de la pagina -->
-    <?php include 'partes/css.php'; ?>
+    <title>Edicion</title>
+    <?php include 'partes/css.php';?>
 </head>
-<body class="px_3">
-    <!-- contenido -->
-    <?php include 'partes/header.php'; ?>
-    <div class="container mt-4">
-        <div class="card border-0">
-            <div class="card-header bg-white border-0">
-                <h1 class="h1">
-                    <i class="bi bi-pencil-fill"></i>DISEÑO
-                </h1>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <img class="w-100" src="resources/img/idea_01.PNG" alt="">
+<body class="px-3">
+    <?php include 'partes/header.php';?>
+        <div class="container mt-4">
+            <div class="card border-0">
+                <div class="card-hearder bg-white border-0">
+                    <h1 class="h1">
+                    <i class="bi bi-airplane"></i>Edicion
+                    </h1>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
+                            <img class="w-100" src="resources/img/idea_03.PNG" alt="">
                     </div>
-                    <div class="col-sm-12 col-md-6">
-                        <p>
-                            Lorem ipsum dolor sit amet 
-                            consectetur adipisicing elit. Minima, 
+                        <div class="col-sm-12 col-md-6">
+                            <p>Lorem ipsum dolor sit amet 
                             optio dolorum sequi, velit ab voluptatibus 
                             minus natus beatae facere modi ex! Impedit, 
                             velit molestias magnam voluptatibus 
@@ -59,13 +55,14 @@
                             fugit blanditiis tenetur error, 
                             ad veniam dolorum labore est, enim qui 
                             fugiat aliquid eos expedita sint soluta 
+                            consectetur adipisicing elit. Minima,
                             laboriosam quisquam.lore
-                        </p>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <?php include 'partes/footer.php'; ?>
+        </div>   
+    <?php include 'partes/footer.php';?>
 </body>
 </html>
